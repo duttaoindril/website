e99f9e423bca1c8bb432caa2670ff8c5a69169b8
duttaoindril <duttaoindril@gmail.com>
2015-01-17 00:01:51 -0800

    Map!

--- js/main.js
+++ js/main.js
@@ -39,85 +39,85 @@ setInterval(function () {
 
 //START AWESOME MAP CODE
 google.maps.event.addDomListener(window, 'load', init);
-var map;
-function init() {
-    var mapOptions = {
-        center: new google.maps.LatLng(37.375467, -121.922283),
-        zoom: 17,
-        zoomControl: true,
-        zoomControlOptions: {
-            style: google.maps.ZoomControlStyle.SMALL,
-        },
-        disableDoubleClickZoom: true,
-        mapTypeControl: true,
-        mapTypeControlOptions: {
-            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-        },
-        scaleControl: false,
-        scrollwheel: false,
-        panControl: true,
-        streetViewControl: true,
-        draggable : false,
-        overviewMapControl: true,
-        overviewMapControlOptions: {
-            opened: true,
-        },
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-    }
-    var mapElement = document.getElementById('map');
-    var map = new google.maps.Map(mapElement, mapOptions);
-    var locations = [
-['PayPal Town Hall', '', '', '', 'http://paypal.com', 37.375467, -121.922283, 'https://mapbuildr.com/assets/img/markers/default.png']
-    ];
-    for (i = 0; i < locations.length; i++) {
-        if (locations[i][1] =='undefined'){ description ='';} else { description = locations[i][1];}
-        if (locations[i][2] =='undefined'){ telephone ='';} else { telephone = locations[i][2];}
-        if (locations[i][3] =='undefined'){ email ='';} else { email = locations[i][3];}
-        if (locations[i][4] =='undefined'){ web ='';} else { web = locations[i][4];}
-        if (locations[i][7] =='undefined'){ markericon ='';} else { markericon = locations[i][7];}
-        marker = new google.maps.Marker({
-            icon: markericon,
-            position: new google.maps.LatLng(locations[i][5], locations[i][6]),
-            map: map,
-            title: locations[i][0],
-            desc: description,
-            tel: telephone,
-            email: email,
-            web: web
+    var map;
+    function init() {
+        var mapOptions = {
+            center: new google.maps.LatLng(37.375467, -121.922283),
+            zoom: 17,
+            zoomControl: true,
+            zoomControlOptions: {
+                style: google.maps.ZoomControlStyle.SMALL,
+            },
+            disableDoubleClickZoom: true,
+            mapTypeControl: true,
+            mapTypeControlOptions: {
+                style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+            },
+            scaleControl: false,
+            scrollwheel: false,
+            panControl: true,
+            streetViewControl: true,
+            draggable : false,
+            overviewMapControl: true,
+            overviewMapControlOptions: {
+                opened: true,
+            },
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+        }
+        var mapElement = document.getElementById('map');
+        var map = new google.maps.Map(mapElement, mapOptions);
+        var locations = [
+['PayPal Town Hall', 'undefined', 'undefined', 'undefined', 'paypal.com', 37.375467,  -121.922283, 'https://mapbuildr.com/assets/img/markers/default.png']
+        ];
+        for (i = 0; i < locations.length; i++) {
+            if (locations[i][1] =='undefined'){ description ='';} else { description = locations[i][1];}
+            if (locations[i][2] =='undefined'){ telephone ='';} else { telephone = locations[i][2];}
+            if (locations[i][3] =='undefined'){ email ='';} else { email = locations[i][3];}
+           if (locations[i][4] =='undefined'){ web ='';} else { web = locations[i][4];}
+           if (locations[i][7] =='undefined'){ markericon ='';} else { markericon = locations[i][7];}
+            marker = new google.maps.Marker({
+                icon: markericon,
+                position: new google.maps.LatLng(locations[i][5], locations[i][6]),
+                map: map,
+                title: locations[i][0],
+                desc: description,
+                tel: telephone,
+                email: email,
+                web: web
+            });
+if (web.substring(0, 7) != "http://") {
+link = "http://" + web;
+} else {
+link = web;
+}
+            bindInfoWindow(marker, map, locations[i][0], description, telephone, email, web, link);
+     }
+ function bindInfoWindow(marker, map, title, desc, telephone, email, web, link) {
+      var infoWindowVisible = (function () {
+              var currentlyVisible = false;
+              return function (visible) {
+                  if (visible !== undefined) {
+                      currentlyVisible = visible;
+                  }
+                  return currentlyVisible;
+               };
+           }());
+           iw = new google.maps.InfoWindow();
+           google.maps.event.addListener(marker, 'click', function() {
+               if (infoWindowVisible()) {
+                   iw.close();
+                   infoWindowVisible(false);
+               } else {
+                   var html= "<div style='color:#000;background-color:#fff;padding:5px;width:150px;'><h4>"+title+"</h4><a href='"+link+"'' >"+web+"<a></div>";
+                   iw = new google.maps.InfoWindow({content:html});
+                   iw.open(map,marker);
+                   infoWindowVisible(true);
+               }
         });
-        bindInfoWindow(marker, map, locations[i][0], description, telephone, email, web);
-    }
-function bindInfoWindow(marker, map, title, desc, telephone, email, web) {
-    if (web.substring(0, 7) != "http://") {
-        link = "http://" + web;
-    } else {
-        link = web;
-    }
-    var infoWindowVisible = (function () {
-          var currentlyVisible = false;
-          return function (visible) {
-              if (visible !== undefined) {
-                  currentlyVisible = visible;
-              }
-              return currentlyVisible;
-           };
-       }());
-       iw = new google.maps.InfoWindow();
-       google.maps.event.addListener(marker, 'click', function() {
-           if (infoWindowVisible()) {
-               iw.close();
-               infoWindowVisible(false);
-           } else {
-               var html= "<div style='color:#000;background-color:#fff;padding:5px;width:150px;'><h4>"+title+"</h4><p>"+desc+"<p><a href='mailto:"+email+"' >"+email+"<a><a href='"+link+"'' >"+web+"<a></div>";
-               iw = new google.maps.InfoWindow({content:html});
-               iw.open(map,marker);
-               infoWindowVisible(true);
-           }
-    });
-    google.maps.event.addListener(iw, 'closeclick', function () {
-        infoWindowVisible(false);
-    });
-    }
+        google.maps.event.addListener(iw, 'closeclick', function () {
+            infoWindowVisible(false);
+        });
+ }
 }
 //END AWESOME MAP CODE
 
