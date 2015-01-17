c6b4d59cf367ad0d86eea395d2e0b8a51c9ff2a7
duttaoindril <duttaoindril@gmail.com>
2015-01-17 08:12:47 -0800

    MAP GONE BATSHITCRAZY!

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ setInterval(function () {
     var current_date = new Date().getTime();
     var seconds_left = (target_date - current_date) / 1000;
     days = parseInt(seconds_left / 86400);
-    seconds_left = seconds_left %  86400;
+    seconds_left = seconds_left % 86400;
     hours = parseInt(seconds_left / 3600);
     seconds_left = seconds_left % 3600;
     minutes = parseInt(seconds_left / 60);
